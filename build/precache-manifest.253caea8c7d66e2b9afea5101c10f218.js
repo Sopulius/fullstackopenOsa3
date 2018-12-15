d64dd62f431575f90cb072584086250691ad4901
Sopulius <aleksi.suoranta@gmail.com>
2018-12-15 14:42:19 +0200

    -3.112

---
+++ build/precache-manifest.253caea8c7d66e2b9afea5101c10f218.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3a83b1e771acc6234bd2",
+    "url": "/static/js/main.3a83b1e7.chunk.js"
+  },
+  {
+    "revision": "bc36c27bea16bffce78b",
+    "url": "/static/js/1.bc36c27b.chunk.js"
+  },
+  {
+    "revision": "3a83b1e771acc6234bd2",
+    "url": "/static/css/main.6677e78b.chunk.css"
+  },
+  {
+    "revision": "91e15a48eb127cb79645e353c9a65b84",
+    "url": "/index.html"
+  }
+];
