4fdac44e6ad5c6f444e3f1178258d34dcbc10e53
Sopulius <aleksi.suoranta@gmail.com>
2018-12-15 16:41:14 +0200

    -3.20

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const morgan = require("morgan")
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(bodyParser.json())
 app.use(morgan('tiny'));
@@ -24,6 +25,14 @@ let persons = [
     id: 4}
 ]
 
+const formatPerson = (person) => {
+    return {
+      name: person.name,
+      number: person.number,
+      id: person._id
+    }
+  }
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
@@ -33,25 +42,30 @@ app.get('/info', (req,res) =>{
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+    Person
+    .find({})
+    .then(persons => {
+      res.json(persons.map(formatPerson))
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if(person){
-        response.json(person)
-    }else{
-        response.status(404).end()
-    }
+    Person
+    .findById(request.params.id)
+    .then(person => {
+      response.json(formatPerson(person))
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+    Person
+    .findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      response.status(400).send({ error: 'malformatted id' })
+    })
   })
 
   app.post('/api/persons', (request, response) => {
@@ -68,15 +82,18 @@ app.delete('/api/persons/:id', (request, response) => {
         return response.status(400).json({error: 'number missing'})
       }
   
-    const person = {
+    const person = new Person({
       name: body.name,
       number: body.number,
       id: Math.floor(Math.random()*10000)
-    }
-  
-    persons = persons.concat(person)
-  
-    response.json(person)
+    })
+    
+    person
+    .save()
+    .then(savedPerson => {
+      response.json(formatPerson(savedPerson))
+    })
+   
   })
 
 const PORT = 3001
---
+++ mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+// korvaa url oman tietokantasi urlilla. ethän laita salasanaa Githubiin!
+const url = ''
+
+mongoose.connect(url)
+
+const Person = mongoose.model('Person', {
+  name: String,
+  number: String
+})
+
+var i = 0
+process.argv.forEach((val, index) => {
+    i++
+});
+
+if(i === 4){
+    var args = process.argv.slice(2)
+    const person = new Person({
+        name: args[0],
+        number: args[1]
+      })
+      
+      person
+        .save()
+        .then(response => {
+          console.log('person saved!')
+          mongoose.connection.close()
+        })
+}else if(i === 2){
+    console.log("Puhelinluettelo:")
+    Person
+  .find({})
+  .then(result => {
+    result.forEach(person => {
+      console.log(person.name + ' ' + person.number)
+    })
+    mongoose.connection.close()
+  })
+}else{
+    console.log("Error: Check the number of arguments.")
+    mongoose.connection.close()
+}
+
